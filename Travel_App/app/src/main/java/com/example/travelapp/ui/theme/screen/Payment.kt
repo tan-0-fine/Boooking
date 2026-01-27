311d6f04eae58a36915c0876d84bfb4cc5d1a9b4
bac21-7 <congbach2107@gmail.com>
2026-01-27 14:50:35 +0700

    Update Payment.kt

--- Travel_App/app/src/main/java/com/example/travelapp/ui/theme/screen/Payment.kt
+++ Travel_App/app/src/main/java/com/example/travelapp/ui/theme/screen/Payment.kt
@@ -362,18 +362,3 @@ fun parsePrice(priceString: String?): Double {
     val cleanString = priceString.replace(Regex("[^0-9]"), "")
     return cleanString.toDoubleOrNull() ?: 0.0
 }
-Paymentmethod
-package com.example.travelapp.ui.theme.data
-
-import com.example.travelapp.R
-
-enum class PaymentMethod(val title: String, val iconRes: Int) {
-    // Lưu ý: Mình đang để tạm hình "ic_launcher_foreground" (hình con robot Android)
-    // để code không bị lỗi đỏ. Bạn hãy thay bằng tên ảnh icon thật của bạn (ví dụ: R.drawable.ic_momo)
-
-    PAY_AT_HOTEL("Thanh toán tại khách sạn", R.drawable.hotel),
-    CREDIT_CARD("Thẻ tín dụng / Ghi nợ", R.drawable.card),
-    BANK_TRANSFER("Chuyển khoản ngân hàng", R.drawable.bank),
-    MOMO("Ví MoMo", R.drawable.momo),
-    ZALOPAY("Ví ZaloPay", R.drawable.zalopay);
-}
